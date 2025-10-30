5e4f2e161dda52ee259f0aabbeea463505b6e65d
Rohit3105 <rgrohitgiri88@gmail.com>
2025-10-30 22:29:25 +0530

    Time: 0 ms (100%), Space: 43.6 MB (18.59%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        Set<Integer> set= new HashSet<>();
-        for(int num: nums){
-            set.add(num);
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++; right--;
+            }
+            else if (nums[left] <= nums[mid]) { 
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else { 
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
-        if(set.contains(target)){
-            return true;
-        }else{
-            return false;
-        }
-        
+        return false;
     }
 }
