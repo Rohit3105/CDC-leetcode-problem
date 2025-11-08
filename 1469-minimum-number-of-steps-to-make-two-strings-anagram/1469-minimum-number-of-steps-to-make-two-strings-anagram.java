71d22de58e896c43c351afc541b10b10c5f95f2f
Rohit3105 <rgrohitgiri88@gmail.com>
2025-11-08 14:34:24 +0530

    Time: 53 ms (21.64%), Space: 47.6 MB (7.91%) - LeetHub

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSteps(String s, String t) {
+        int count =0;
+        Map<Character,Integer> map1 = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        }
+         Map<Character,Integer> map2 = new HashMap<>();
+         for(char ch : t.toCharArray()){
+             map2.put(ch,map2.getOrDefault(ch,0)+1);
+         }
+         for(char ch : map1.keySet()){
+            if(map2.containsKey(ch)){
+                if(map1.get(ch)>map2.get(ch)){
+                    count += map1.get(ch)-map2.get(ch);
+                }
+            }else{
+                count += map1.get(ch);
+
+            }
+         }
+         return count;
+        
+    }
+}
