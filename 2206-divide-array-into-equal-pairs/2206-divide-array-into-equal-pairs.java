337c65af4eb141aab07b00bb5f89285ea6980bc4
Rohit3105 <rgrohitgiri88@gmail.com>
2025-12-22 14:37:47 +0530

    Time: 8 ms (46.73%), Space: 46.4 MB (56.83%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        int n = nums.length/2;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int num:map.keySet()){
+            if(map.get(num)%2!=0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
