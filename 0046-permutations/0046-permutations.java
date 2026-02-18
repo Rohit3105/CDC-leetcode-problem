8b794da99e6a0e9d99b618c50b9e7b26d975cadf
Rohit Giri <rgrohitgiri88@gmail.com>
2026-02-19 01:12:49 +0530

    Time: 1 ms (85.9%), Space: 45.4 MB (90.34%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,22 @@
+class Solution {
+    ArrayList<List<Integer>> res = new ArrayList<>();
+    public void solve(int[] nums,boolean[] used,List<Integer> list){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(used[i]) continue;
+            used[i] = true;
+
+            list.add(nums[i]);
+            solve(nums, used,list);
+            list.remove(list.size()-1);
+            used[i] = false;
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        solve(nums,new boolean[nums.length],new ArrayList<>());
+        return res;
+    }
+}
