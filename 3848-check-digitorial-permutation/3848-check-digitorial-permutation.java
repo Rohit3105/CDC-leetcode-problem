34f3167f1c4441fdc2ad1a59a56cc2f4c38a718f
Rohit Giri <rgrohitgiri88@gmail.com>
2026-02-23 11:52:10 +0530

    Time: 3 ms (0%), Space: 43.3 MB (0%) - LeetHub

---
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int fact(int num){
+        int factorial = 1;
+        for(int i =1; i<=num; i++){
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public boolean isDigitorialPermutation(int n) {
+        int original = n;
+        int sum = 0;
+        while (n != 0) {
+            int num = n % 10;
+            sum += fact(num);
+            n = n / 10;
+        }
+     char s[] = Integer.toString(original).toCharArray();
+     char ss[] = Integer.toString(sum).toCharArray();
+     Arrays.sort(s);
+     Arrays.sort(ss);
+        if(Arrays.equals(s,ss)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
