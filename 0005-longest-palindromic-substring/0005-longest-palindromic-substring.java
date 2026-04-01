7f587d6b48b7a455d87cd7b9edcf58b1a737da3e
Rohit Giri <rgrohitgiri88@gmail.com>
2026-04-01 18:01:15 +0530

    Time: 247 ms (17.34%), Space: 120.2 MB (5.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,15 +1,26 @@
 class Solution {
+    int[][] dp;
     public boolean isSubstring(String s, int i, int j){
         if(i>j){
             return true;
         }
+        if(dp[i][j]!=-1){
+            return dp[i][j] == 1;
+        }
         if(s.charAt(i)!=s.charAt(j)){
+            dp[i][j] = 0;
             return false;
         }
-        return isSubstring(s,i+1,j-1);
+        boolean res =  isSubstring(s,i+1,j-1);
+        dp[i][j] = res?1:0;
+        return res;
     }
     public String longestPalindrome(String s) {
         int n =s.length();
+        dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            Arrays.fill(dp[i],-1);
+        }
         int max = Integer.MIN_VALUE;
         String ans = "";
         for(int i=0; i<n; i++){
