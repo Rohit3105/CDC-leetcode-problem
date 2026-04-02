97a1245295c0b385b44eb89fbac700ee5396d07b
Rohit Giri <rgrohitgiri88@gmail.com>
2026-04-02 21:54:23 +0530

    Time: 236 ms (17.65%), Space: 176.7 MB (5.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,27 @@
 class Solution {
-    int[][] dp;
-    public boolean isSubstring(String s, int i, int j){
+    Boolean[][] dp;
+    public boolean check(String s, int i,  int j){
         if(i>j){
             return true;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j] == 1;
-        }
+        if(dp[i][j]!=null) return dp[i][j];
         if(s.charAt(i)!=s.charAt(j)){
-            dp[i][j] = 0;
-            return false;
+            return dp[i][j] = false;
         }
-        boolean res =  isSubstring(s,i+1,j-1);
-        dp[i][j] = res?1:0;
-        return res;
+        return dp[i][j] = check(s,i+1,j-1);
+
     }
     public String longestPalindrome(String s) {
-        int n =s.length();
-        dp = new int[n][n];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i],-1);
-        }
-        int max = Integer.MIN_VALUE;
+         int n = s.length();
+        dp = new Boolean[n][n];
+        int max = 0;
         String ans = "";
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(isSubstring(s,i,j)){
-                    int sz= j-i+1;
-                    if(max<sz){
-                        max = sz;
+        for(int i =0; i<n; i++){
+            for(int j =i; j<n; j++){
+                if(check(s,i,j)){
+                    int len = j-i+1;
+                    if(max<len){
+                        max = len;
                         ans = s.substring(i,j+1);
                     }
                 }
