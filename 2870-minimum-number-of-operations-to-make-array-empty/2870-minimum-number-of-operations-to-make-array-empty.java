3b894ab75257260f1be452e85f03ee099650fa99
Rohit Giri <rgrohitgiri88@gmail.com>
2026-05-04 22:38:37 +0530

    Time: 20 ms (90.43%), Space: 90 MB (11.22%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,25 +1,19 @@
 class Solution {
     public int minOperations(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
-        
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        
-        int steps = 0;
-        
+        int steps =0;
         for(int i:map.values()){
-            if(i == 1){
-                return -1;  // ❌ single element can't be removed
-            }
-            else if(i == 2 || i == 3){
-                steps += 1; // ✅ already correct
-            }
-            else{
-                steps += (i + 2) / 3;  // ✅ FIXED HERE
-            }
+           if(i==1) return -1;
+           else if(i>3){
+           steps += (i+2)/3;
+           }
+           else{
+            steps += 1;
+           }
         }
-        
         return steps;
     }
 }
